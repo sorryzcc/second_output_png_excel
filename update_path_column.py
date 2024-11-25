a9d9110391bba923a3ad88b930538d40ec7c30a0
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2024-11-25 09:17:44 +0800

    修改脚本名称

--- update_path_column.py
+++ update_path_column.py
@@ -17,7 +17,7 @@ def update_paths(input_file, output_file, base_path):
 # 主函数
 if __name__ == "__main__":
     input_file = 'log.xlsx'
-    output_file = 'updated_processed_svn_log.xlsx'
+    output_file = 'update_path_column.xlsx'
     base_path = r'I:\PM_Mainland_Trunk_20230321_r552586\PMGameClient\PMGame'
     
     # 更新路径并保存
